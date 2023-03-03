009a05c8e0e39caa123ad63988783a51e52013c4
jakeqiu <13437965971@163.com>
2023-03-04 07:44:40 +0800

    修改出库时间及单号的问题

--- src/api/routers/updateMsg.js
+++ src/api/routers/updateMsg.js
@@ -2,12 +2,11 @@
 const Router = require("koa-router");
 const router = new Router();
 const db = require('../../db/database');
- 
+ const ObjectID = require("mongodb").ObjectId
 router.get('/',async(ctx,next)=>{
-    let {No,address,bianhao,contact,lister,name,phone,status} = ctx.query;
-    
-    let res = await db.update('delivery',{No},{$set:{address,bianhao,contact,lister,name,phone,status}})
-    console.log(res)
+    let {No,address,bianhao,contact,lister,name,phone,status,_id} = ctx.query;
+    _id = new ObjectID(_id)
+    let res = await db.update('delivery',{_id},{$set:{address,bianhao,contact,lister,name,phone,status,No}})
     if(res.modifiedCount>0){
         ctx.body = {
             state: 200,
